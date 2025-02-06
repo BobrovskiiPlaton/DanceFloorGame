c979b2d76bfcda781dae0f879c6dd575c6ef9b5a
Demyan <demian.bob@gmail.com>
2025-02-06 14:14:44 +0300

    Added level selector

--- AllColors/AllColors/Assets/Scripts/MoveObject.cs
+++ AllColors/AllColors/Assets/Scripts/MoveObject.cs
@@ -7,8 +7,7 @@ public class MoveObject : MonoBehaviour
     public RectTransform[] targetTransforms; // Массив целевых объектов
     public Vector3 moveDistance; // Расстояние, на которое нужно переместить объекты
     public float speed = 2.0f; // Скорость перемещения
-
-    private bool shouldMove = false;
+    
     private Vector3[] initialPositions;
 
     void Start()
@@ -20,25 +19,12 @@ public class MoveObject : MonoBehaviour
         }
     }
 
-    void Update()
-    {
-        if (shouldMove)
-        {
-            for (int i = 0; i < targetTransforms.Length; i++)
-            {
-                targetTransforms[i].anchoredPosition = Vector3.Lerp(targetTransforms[i].anchoredPosition, initialPositions[i] + moveDistance, speed * Time.deltaTime);
-            }
-
-            if (Vector3.Distance(targetTransforms[0].anchoredPosition, initialPositions[0] + moveDistance) < 0.01f)
-            {
-                shouldMove = false;
-            }
-        }
-    }
-
     // Метод для начала перемещения объектов
     public void StartMoving()
     {
-        shouldMove = true;
+        for (int i = 0; i < targetTransforms.Length; i++)
+        {
+            targetTransforms[i].anchoredPosition = Vector3.Lerp(targetTransforms[i].anchoredPosition, initialPositions[i] + moveDistance, speed * Time.deltaTime);
+        }
     }
 }
